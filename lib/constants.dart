9f3c1581b4b53dc90d45b834c09d058bf270717d
ashishbinu <ashishbinu90@gmail.com>
2020-09-19 19:39:16 +0530

    added google fonts and corrected colors in constant.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-const Color kDarkBlue = const Color(0XFFDEEAF8);
+const Color kDarkBlue = const Color(0XFF263145);
 const Color kDarkGrey = const Color(0XFF333F55);
 const Color kGrey = const Color(0XFF687C94);
 const Color kLightGrey = const Color(0XFFB3C6EA);
 const Color kWhite = const Color(0XFFDEEAF8);
 const Color kPink = const Color(0XFFF6376D);
+
+
