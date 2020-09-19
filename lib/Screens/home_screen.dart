22b5500b6ad0ecc912dc2acd37820c3cfd3923f9
AJAYK-01 <ajayk@cet.ac.in>
2020-09-19 15:23:21 +0530

    posting added

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,12 +1,27 @@
+import 'package:HackMIT_Project/Services/posting.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String route = 'HomeScreen';
+  final Posting posting = Posting();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Container(
+          // child: FlatButton(
+          //     onPressed: () async {
+          //       // await posting.addPost(
+          //       //     username: 'ajay',
+          //       //     post: 'this post',
+          //       //     why1: 'hello',
+          //       //     why2: 'hhehe',
+          //       //     why3: 'plss');
+          //     await posting.deletePost(
+          //           username: 'ajay', docId: 'Bwim9RHEZ6ZMQeEGBdYE');
+          //     },
+          //     child: Text('test')),
+          ),
     );
   }
 }
